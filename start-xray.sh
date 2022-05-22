21145b296dea7627a0658398ba02f20b6b609d5b
Genghis Yang <yangcheng2503@163.com>
2022-05-22 21:53:41 +0800

    For testing

--- start-xray.sh
+++ start-xray.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -exuo pipefail
 
-echo -n ${XRAY_CONFIG} | base64 -d | sed "s/\"port\":\ 443,/\"port\": ${PORT},/g" > /usr/local/etc/xray/config.json
+echo -n ${XRAY_CONFIG} > config.bash64
+
+echo -n ${XRAY_CONFIG} | base64 -d | sed "s/\"port\":\ 443,/\"port\": ${PORT},/g" > /usr/local/etc/xray/config.json && \
 xray --config /usr/local/etc/xray/config.json
